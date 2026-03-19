6223a0829b497c81ed41346e1164901615e9b62d
Smonique <121874599+Smonique@users.noreply.github.com>
2026-03-19 20:17:08 +0400

    servives

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React, { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import TrustBy from './components/TrustBy'
+import Services from './components/Services'
 function App() {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
   return (
     <div className='dark:bg-black relative'>
       <Navbar theme={theme} setTheme={setTheme} />
-      <Hero/>
+      <Hero />
+      <TrustBy />
+      <Services/>
     </div>
   )
 }
