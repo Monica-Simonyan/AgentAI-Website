0e00cbc81d55e7700219fe75322d37e39aa34fc9
Monica-Simonyan <121874599+Smonique@users.noreply.github.com>
2026-03-21 15:41:24 +0400

    motion

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import Title from './Title'
 import assets, { teamData } from '../assets/assets'
-
+import { motion } from 'motion/react'
 const Teams = () => {
   return (
-      <div className='flex flex-col items-center gap-7 px-4 sm:px-12
-      lg:pc-24 xl:px-40 pt-30 text-gray-800 dark:text-white' id="teams">
+      <motion.div className='flex flex-col items-center gap-7 px-4 sm:px-12
+      lg:pc-24 xl:px-40 pt-30 text-gray-800 dark:text-white' id="teams"
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}>
           <Title title='Meet the team' desc='Passioante team of digital experts dedicated to your brand success.' />
           <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-5'>
               {teamData.map((team, index) => (
-                  <div key={index} className='flex max-sm:flex-col items-center gap-5 p-4 rounded-xl border border-gray-100 dark:border-gray-700 bg-white dark:bg-gray-900 shadow-xl shadow-gray-100 dark:shadow-white/5 hover:scale-103 transition-all duration-400'>
+                  <motion.div key={index} className='flex max-sm:flex-col items-center gap-5 p-4 rounded-xl border border-gray-100 dark:border-gray-700 bg-white dark:bg-gray-900 shadow-xl shadow-gray-100 dark:shadow-white/5 hover:scale-103 transition-all duration-400'>
                       <img src={ team.image} className='w-12 h-12 rounded-full'/>
-                      <div className='flex-1'>
+                      <div className='flex-1'
+                      initial={{ opacity: 0, y: 20 }}
+                      whileInView={{ opacity: 1, y: 0 }}
+                      transition={{ duration: 0.4,delay:index*0.1}}
+                      viewport={{once:true}}                      >
                           <h3 className='font-bold txt-sm'>{team.name}</h3>
                           <p className='text-xs opacity-60'>{ team.title}</p>
                       </div>
-                  </div>
+                  </motion.div>
               ))}
           </div>
-    </div>
+    </motion.div>
   )
 }
 
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-
+import { motion } from 'motion/react'
 const Title = ({title, desc}) => {
   return (
      <>
-          <h2 className='text-3xl sm:text-5xl font-medium'>
+      <motion.h2 className='text-3xl sm:text-5xl font-medium'
+         initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6}}
+          viewport={{once:true}}>
               {title}
-          </h2>  
-          <p className='max-w-lg text-center text-gray-500 dark:text-white/75 mb-6'>
+          </motion.h2>  
+      <motion.p className='max-w-lg text-center text-gray-500 dark:text-white/75 mb-6'
+      initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5,delay:0.2}}
+          viewport={{once:true}}>
               {desc}
-          </p>
+          </motion.p>
     </>
   )
 }
