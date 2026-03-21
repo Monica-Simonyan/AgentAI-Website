a21cdafcd9348fdc9b1a7d353389267e8d62330a
Monica-Simonyan <121874599+Smonique@users.noreply.github.com>
2026-03-21 15:59:20 +0400

    finish

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = ({theme}) => {
           <div className='flex justify-between lg:items-center max-lg:flex-col gap-10'>
               <motion.div className='space-y-5 text-sm text-gray-700 dark:text-gray-400'
               initial={{ opacity: 0, x: -30 }}
-              whileInView={{ opacity: 1, y: 0 }}
+              whileInView={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.6, delay:0.2}}
               viewport={{once:true}}>
                   <img src={theme === 'dark' ? assets.logo_dark : assets.logo} className='w-32 sm:w-44' alt=''/>
